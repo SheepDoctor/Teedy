d48f6a723f503ac782f4a4cbc9e5743fd27546ca
Bingwen Dong <12211111@mail.sustech.edu.cn>
2025-05-14 13:45:07 +0800

    514

---
+++ scripts/docker.sh
@@ -0,0 +1,3 @@
+docker run -d -p 8084:8080 --name teedy_manual01 teedy2025_manual
+docker run -d -p 8083:8080 --name teedy_manual02 teedy2025_manual
+docker run -d -p 8082:8080 --name teedy_manual03 teedy2025_manual
